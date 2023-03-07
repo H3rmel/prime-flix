054a3091c8bba49d88d36b992d80751f3da65642
Isaac Hermel <isaachermel@gmail.com>
2023-03-07 01:23:34 -0300

    need to be fixed

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { Error, Favorites, Home, Movie, NotFound } from "../pages";
+import { Error, Favorites, Home, Movie, NotFound } from "///pages";
 
 export const router = createBrowserRouter([
   {
